6b4c5a75ba39d6ea8f5f882d396bd0690b752392
OmegaZerg <ragged17@Bellsouth.net>
2025-05-18 10:37:17 -0500

    Added shell script to run program. Added gitignore file. Modified existing Window class, and added Point and Line classes to display file. Created a window in main, along with a couple points and a line to test current functionality.

--- display.py
+++ display.py
@@ -1,5 +1,18 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point():
+    def __init__(self, x: float, y: float):
+        self.x_coordinate = x
+        self.y_coordinate = y
+
+class Line():
+    def __init__(self, point1: Point, point2: Point):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(self.point1.x_coordinate, self.point1.y_coordinate, self.point2.x_coordinate, self.point2.y_coordinate, fill=fill_color, width=2)
+
 class Window():
     def __init__(self, width: int, height: int):
         self.__width = width
@@ -15,6 +28,9 @@ class Window():
         self.__root.update()
         self.__root.update_idletasks()
 
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.__canvas, fill_color)
+
     def wait_for_close(self):
         self.__is_running = True
         while self.__is_running:
@@ -24,3 +40,4 @@ class Window():
     def close(self):
         self.__is_running = False
         
+
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from display import Window
+from display import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    point1, point2 = Point(100.5, 250), Point(300, 320.8)
+    line = Line(point1, point2)
+    win.draw_line(line, "black")
+    win.redraw()
+
     win.wait_for_close()
 
 if __name__ == "__main__":
